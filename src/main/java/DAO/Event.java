7c9eeb8afc5a48587ef8c441577a2872dd9055d4
saroj kumar sah <sarojahah152@gmail.com>
2023-12-28 00:26:54 +0545

    change

--- src/main/java/DAO/Event.java
+++ src/main/java/DAO/Event.java
@@ -3,6 +3,8 @@ package DAO;
 import Model.EventsModel;
 import Model.UserId;
 import database.DatabaseConnection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 
 public class Event extends DatabaseConnection {
@@ -16,8 +18,27 @@ public class Event extends DatabaseConnection {
     }
 private int userid;
     public void CreateEvent(EventsModel event) {
-        String query = "INSERT INTO events(IMAGE,NAME,PRICE,DESCRIPTION,PLACE,CAPACITY,ISPUBLIC,USERID,VENUEID,date) VALUES('" + Arrays.toString(event.getImage()) + "','" + event.getEvent_name() + "','" + event.getPrice() + "','" + event.getDescription() + "','" + event.getVanue() + "','" + event.getCapacity() + "','" + 0 + "'," + userid + "','" + 1 + "','" + event.getDate() + "')";
-        manipulate(query);
+//        String query = "INSERT INTO events(IMAGE,NAME,PRICE,DESCRIPTION,PLACE,CAPACITY,ISPUBLIC,USERID,VENUEID,date) VALUES('" + Arrays.toString(event.getImage()) + "','" + event.getEvent_name() + "','" + event.getPrice() + "','" + event.getDescription() + "','" + event.getVanue() + "','" + event.getCapacity() + "','" + 0 + "'," + userid + "','" + 1 + "','" + event.getDate() + "')";
+//        manipulate(query);
+System.out.println(userid);
+String query = "INSERT INTO events (IMAGE, NAME, PRICE, DESCRIPTION, PLACE, CAPACITY, ISPUBLIC, USERID, VENUEID, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+    preparedStatement.setBytes(1, event.getImage());
+    preparedStatement.setString(2, event.getEvent_name());
+    preparedStatement.setInt(3, event.getPrice());
+    preparedStatement.setString(4, event.getDescription());
+    preparedStatement.setString(5, event.getVanue());
+    preparedStatement.setInt(6, event.getCapacity());
+    preparedStatement.setInt(7, 0);  // Assuming ISPUBLIC is an integer
+    preparedStatement.setInt(8, userid);
+    preparedStatement.setInt(9, 1);  // Assuming VENUEID is an integer
+    preparedStatement.setDate(10, java.sql.Date.valueOf(event.getDate()));
+
+    preparedStatement.executeUpdate();
+} catch (SQLException e) {
+    e.printStackTrace();
+}
+
 
     }
 
